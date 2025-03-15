69102412f4a96e5c4e012596801adb38d9c01b11
Paweł Pozorski <pozorski.paul@gmail.com>
2025-03-15 14:55:54 +0100

    Initial Commit

---
+++ Imagy/ImagyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagyApp.swift
+//  Imagy
+//
+//  Created by Pawel P on 15/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ImagyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImagyTests/ImagyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImagyTests.swift
+//  ImagyTests
+//
+//  Created by Pawel P on 15/03/2025.
+//
+
+import Testing
+@testable import Imagy
+
+struct ImagyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
